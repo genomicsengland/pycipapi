127df04c3764bd692d90c7deae244a3890eadc66
antonior26 <aruemar@gmail.com>
2018-09-20 14:13:48 +0100

    changed retries to 1000

--- pycipapi/rest_client.py
+++ pycipapi/rest_client.py
@@ -26,7 +26,7 @@ class BlockedCase(HTTPError):
     pass
 
 
-def requests_retry_session(retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504, 503), session=None):
+def requests_retry_session(retries=1000, backoff_factor=0.8, status_forcelist=(500, 502, 504, 503), session=None):
     session = session or requests.Session()
     retry = Retry(
         total=retries,
